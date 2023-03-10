bda5889bf9d8b3b1d2747a3acd73b34fac51ca7b
avasdowney <avasdowney@gmail.com>
2023-03-10 15:55:06 -0500

    added urls for all pages

--- main/views.py
+++ main/views.py
@@ -5,4 +5,28 @@ def index(request):
     return HttpResponse("Login Page")
 
 def login(request):
-    return HttpResponse("UserLogin")
+    return HttpResponse("User Login")
+
+def create_account(request):
+    return HttpResponse("Create Account")
+
+def news(request):
+    return HttpResponse("News")
+
+def directory(request):
+    return HttpResponse("Directory")
+
+def member_profile(request):
+    return HttpResponse("Member Profile")
+
+def admin_dashboard(request):
+    return HttpResponse("Admin Profile")
+
+def treasurer_dashboard(request):
+    return HttpResponse("Treasurer Profile")
+
+def payment(request):
+    return HttpResponse("Payment")
+
+def reserve_court(request):
+    return HttpResponse("Reserve Court")
