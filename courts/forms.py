1c8b8ad3b85d410111ec25325574ffe863b9f3f1
AntTran56 <anthonytran116@yahoo.com>
2023-03-31 16:00:55 -0400

    added missing half of changes

--- courts/forms.py
+++ courts/forms.py
@@ -6,3 +6,14 @@ class ReservationForm(ModelForm):
     class Meta:
         model = courtReservationForm
         fields = '__all__'
+        labels = {
+            'guest1EMail' : ("Guest 1's Email Address"),
+            'guest1FName': ("Guest 1's First Name"),
+            'guest1LName': ("Guest 1's Last Name"),
+            'guest2EMail' :("Guest 2's Email Address"),
+            'guest2FName' : ("Guest 2's First Name"),
+            'guest2LName': ("Guest 1's Last Name"),
+            'guest3EMail' : ("Guest 3's Email Address"),
+            'guest3FName' : ("Guest 3's First Name"),
+            'guest3LName': ("Guest 3's Last Name")
+        }
--- courts/models.py
+++ courts/models.py
@@ -3,12 +3,12 @@ from django.db import models
 # Create your models here.
 
 class courtReservationForm(models.Model):
-    guest1EMail = models.EmailField(max_length=254)
-    guest1FName = models.CharField(max_length=100)
-    guest1LName = models.CharField(max_length=100)
-    guest2EMail = models.EmailField(max_length=254)
-    guest2FName = models.CharField(max_length=100)
-    guest2LName = models.CharField(max_length=100)
-    guest3EMail = models.EmailField(max_length=254)
-    guest3FName = models.CharField(max_length=100)
-    guest3LName = models.CharField(max_length=100)
+    guest1EMail = models.EmailField(max_length=254, blank = True)
+    guest1FName = models.CharField(max_length=100, blank = True)
+    guest1LName = models.CharField(max_length=100, blank = True)
+    guest2EMail = models.EmailField(max_length=254, blank = True)
+    guest2FName = models.CharField(max_length=100, blank = True)
+    guest2LName = models.CharField(max_length=100, blank = True)
+    guest3EMail = models.EmailField(max_length=254, blank = True)
+    guest3FName = models.CharField(max_length=100, blank = True)
+    guest3LName = models.CharField(max_length=100, blank = True)
