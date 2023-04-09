0a63ffc10cb068251771f221592ca09c6357748a
avasdowney <avasdowney@gmail.com>
2023-04-09 16:38:08 -0400

    completed signup form

--- members/views.py
+++ members/views.py
@@ -23,11 +23,4 @@ def signup(request):
     return render(request, 'signup.html', {'form': form})
 
 def index(request):
-    if request.method == 'POST':
-        form = MembersForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return render(request, 'members/success.html')
-    form = MembersForm()
-    context = {'form': form}
-    return render(request, 'members/members.html', context)
+    return render(request, 'signup.html', context)
