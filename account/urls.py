d04749e2a136190dd189ad67fe511ff281c9b580
cgaulin <cgaulin@hartford.edu>
2023-04-10 16:58:34 -0400

    added account info and admin info

--- account/urls.py
+++ account/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.account, name='index'),
     path('bills', views.billing, name="billing"),
-
+    path('editProfile', views.editProfile, name="editProfile"),
+    path('adminProfile', views.adminProfile, name="adminProfile"),
 ]
