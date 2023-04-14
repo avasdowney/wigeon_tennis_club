30ff33b5b80989f79dfbf51dacdfe6ff325f5ed1
avasdowney <avasdowney@gmail.com>
2023-04-13 21:44:40 -0400

    started working on test cases

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
-# Create your tests here.
+from news.models import AddNews
+from django.utils import timezone
+from news.forms import AddNewsForm
+
+
+class AddNewsTest(TestCase):
+    
+    def create_news(self, title="only a test", story="yes, this is only a test"):
+        return AddNews.objects.create(title=title, story=story)
+
+    def test_news_title_creation(self):
+        news = self.create_news()
+        self.assertTrue(isinstance(news, AddNews))
+        self.assertEqual("only a test", news.title)
+
+    def test_news_story_creation(self):
+        news = self.create_news()
+        self.assertTrue(isinstance(news, AddNews))
+        self.assertEqual("yes, this is only a test", news.story)
